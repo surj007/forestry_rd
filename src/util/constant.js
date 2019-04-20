546159bd3d3327715cda44388ef649579924ed5a
surj007 <47058214+surj007@users.noreply.github.com>
2019-04-20 19:07:19 +0800

    Update constant.js

--- src/util/constant.js
+++ src/util/constant.js
@@ -8,8 +8,11 @@ exports.CODE_SERVERERR = 6;
 exports.CODE_DUPLICATE = 7;
 exports.CODE_CHANGEPWDERR = 10;
 
-exports.KEY_ACCESSKEYID = 'LTAIUR8UPrDx2ABa';
-exports.KEY_SECRETACCESSKEY = 'gEpafGvrP0uaLwNSH1LZhloIpNgTcv';
+exports.KEY_ACCESSKEYID = 'xxx';
+exports.KEY_SECRETACCESSKEY = 'xxx';
+
+// LTAIUR8UPrDx2ABa
+// gEpafGvrP0uaLwNSH1LZhloIpNgTcv
 
 exports.OSS_BUCKET = 'forestry-bureau';
 exports.OSS_REGION = 'oss-cn-shanghai';
